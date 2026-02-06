a477db563d00dd41f19872e31de0ecbeccb3311a
1xreniergonzalez-sys <1xreniergonzalez@gmail.com>
2026-02-06 09:29:15 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("FIT·AI activa");
+// Registrar Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js")
+    .then(() => {
+      console.log("Service Worker registrado");
+    });
+}
+
+// Notificaciones
+const status = document.getElementById("status");
+const btn = document.getElementById("testBtn");
+
+btn.addEventListener("click", () => {
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      new Notification("FIT·AI", {
+        body: "Notificación funcionando correctamente 💪"
+      });
+      status.textContent = "Notificación enviada ✅";
+    } else {
+      status.textContent = "Permiso denegado ❌";
+    }
+  });
+});
