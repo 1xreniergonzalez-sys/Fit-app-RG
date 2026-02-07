03a4ccf7002cb16e999de98e9dd689c15ba728ae
1xreniergonzalez-sys <1xreniergonzalez@gmail.com>
2026-02-07 02:20:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -192,5 +192,20 @@ function saveCompletedExercise() {
   let done = Number(localStorage.getItem("done")) || 0;
   localStorage.setItem("done", done + 1);
 }
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll(".card:not(.dragging)")];
 
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child };
+    } else {
+      return closest;
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+   
 });
